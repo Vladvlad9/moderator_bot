8af48e84d92a65533d8760745209e139579f5aab
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-24 22:17:39 +0300

    создание handler для проверки стикеров и удаление создание handler для проверки отправленого сообщения на запрещенные слова

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,15 +1,22 @@
+import string
+
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
 from loader import dp
+import json
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!')
 
+@dp.message_handler(content_types=['sticker'])
+async def echo_send(message: types.Message):
+    await message.delete()
 
 @dp.message_handler()
 async def echo_send(message: types.Message):
-    #if message.text
-    pass
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()} \
+            .intersection(set(json.load(open('cenz.json')))):
+        await message.delete()
