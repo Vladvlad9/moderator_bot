ed86b2b5f040f00e2146bfdb0bc7fbd6dd9f1327
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-31 19:05:23 +0300

    Проверна на наличие admin в чате

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,22 +1,15 @@
-import string
-
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
+from filters.is_admin import IsMe
 
 from loader import dp
-import json
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f'Привет, {message.from_user.full_name}!')
 
-@dp.message_handler(content_types=['sticker'])
-async def echo_send(message: types.Message):
-    await message.delete()
 
-@dp.message_handler()
-async def echo_send(message: types.Message):
-    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split()} \
-            .intersection(set(json.load(open('cenz.json')))):
-        await message.delete()
+@dp.message_handler(IsMe())
+async def admin_hello(message: types.Message):
+    await message.answer('you admin')
