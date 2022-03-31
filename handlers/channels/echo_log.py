78b593c00deac5b17290e1e8e7aadd511d2a84c6
Vlad Paulechka <vlad9.ru@gmail.com>
2022-03-31 19:06:58 +0300

    Проверка что пользователь написал под постом

---
+++ handlers/channels/echo_log.py
@@ -0,0 +1,17 @@
+from aiogram import types
+
+from loader import dp
+
+
+@dp.message_handler(chat_type=types.ChatType.SUPERGROUP)
+async def log(message: types.Message):
+    if message.from_user.first_name != 'Telegram':
+        # сюда попадают комменты
+        print(message.text) # текст коммента
+        print(message.from_user.id) # id user
+        print(message.from_user.username) # username
+        print(message.reply_to_message.message_id) # id posta
+    else:
+        # сюда попадают посты
+        print(message.message_id) # id posta
+        print(message.text) # text posta
