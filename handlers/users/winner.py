080d4f60d3492514a9da52ee9787320d788b83ed
Vlad Paulechka <vlad9.ru@gmail.com>
2022-04-04 14:38:16 +0300

    в функции count_users происходит выбор по id_post

--- handlers/users/winner.py
+++ handlers/users/winner.py
@@ -8,7 +8,12 @@ import random
 
 @dp.message_handler(text="Выбрать победителя")
 async def back_main_menu(message: types.Message):
-    count_users = await db.count_users('Самый первый пост')
+
+    id_post = await db.get_id_post()
+    count_users = await db.count_users(int(id_post[0][0]))
+
     random_user = random.randint(1, int(count_users[0]))
-    user_winner = await db.user_winner(random_user, 'Самый первый пост')
+
+    user_winner = await db.user_winner(random_user, int(id_post[0][0]))
+
     await message.answer(f'Победил пользователь: {user_winner[0][0]} c комментарием {user_winner[0][1]}')
